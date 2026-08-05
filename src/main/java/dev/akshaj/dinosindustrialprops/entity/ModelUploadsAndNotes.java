8f96a36fab42719f3f657148950d549500af2e76
Dinoninja_27 <tmlemke2022@gmail.com>
2026-08-05 17:06:39 -0400

    Uploaded models and textures for the first batch of models (traffic cones).

---
+++ src/main/java/dev/akshaj/dinosindustrialprops/entity/ModelUploadsAndNotes.java
@@ -0,0 +1,16 @@
+package dev.akshaj.dinosindustrialprops.entity;
+
+public record ModelUploadsAndNotes() {
+}
+
+
+//models added 8/5/2026
+//notes: all cones (and cone cart) should be able to rotate on 15 degree intervals when placed, like a mob head
+//cone cart acts like a stonecutter, and all cone types should be able to be turned into each other via the menu
+//TrafficCone_Test serves as the base model to put into the cone cart, crafted vertically with 2 orange wool surrounding 1 white wool
+
+//list of models:
+//Cone_Cart, DriversED_Cone, Parking_Cone, Tall_Thin_Cone, Thin_Cone, Traffic_Barrel, Traffic_Pole
+//TrafficCone_Medium (and stacks 3,6,+8), TrafficCone_Short (stacks 2,4,+7), TrafficCone_Tall (stacks 3,5,+7)
+//TrafficCone_Test, Vertical_Panel_Plasticade1
+
